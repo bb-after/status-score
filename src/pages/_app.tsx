d14f57ce76d3d737b66ebf308b494f8a7123e776
bb-after <117300088+bb-after@users.noreply.github.com>
2024-11-26 12:25:46 -0500

    updated home apge to be more on brand.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,18 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import theme from "../theme/theme";
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
-      <ChakraProvider>
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-      </QueryClientProvider>
+      <ChakraProvider theme={theme}>
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+        </QueryClientProvider>
       </ChakraProvider>
     </UserProvider>
   );
