1d1a10019ff9d154eacf9a03c5cf1ea41c4c5fb1
bb-after <117300088+bb-after@users.noreply.github.com>
2024-11-27 11:38:06 -0500

    fixed primsa error

--- src/pages/api/keywords/index.ts
+++ src/pages/api/keywords/index.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '../../../lib/prisma';
 import { getAuthenticatedUserId } from '../../../lib/auth';
+import { Prisma } from '@prisma/client';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
