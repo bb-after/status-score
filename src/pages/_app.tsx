7ffc2139784d09e7431087d4095dae9816524d66
bb-after <117300088+bb-after@users.noreply.github.com>
2024-12-09 13:18:21 -0500

    updates to _app.tsx before ultimate removal.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { AuthProvider } from "../contexts/AuthContext";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import theme from "../theme/theme";
@@ -10,9 +11,13 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
       <ChakraProvider theme={theme}>
-        <QueryClientProvider client={queryClient}>
-          <Component {...pageProps} />
-        </QueryClientProvider>
+        <AuthProvider
+          value={{ isAuthenticated: false, isLoading: true, user: null }}
+        >
+          <QueryClientProvider client={queryClient}>
+            <Component {...pageProps} />
+          </QueryClientProvider>
+        </AuthProvider>
       </ChakraProvider>
     </UserProvider>
   );
